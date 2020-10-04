472a3e18bd390de4c5ab62b950aa7f530fe1cf6a
Chris Hambacher <hambcastlesen12@gmail.com>
2020-10-04 10:22:55 -0400

    Added initial code for managers.
    Added a few scripts that serve as the base for the managers. Added fields and methods to the Enemy and EnemyManager class.

---
+++ UnityProject/Assets/Scripts/Enemy.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Enemy class
+// - Contains all the information on each enemy
+// - Does the path calculations locally using an internal timer.
+
+public class Enemy
+{
+    private readonly EnemyManager enemyManager;
+    private float timer;                // The amount of time this enemy has been alive.
+
+    private int health;                 // Heath of the unit
+    private Vector3 position;           // Current position on the screen.
+    private int targetPositionIndex;    // The index in the position list of the target position.
+    private float timePerPosition;      // The amount of time it takes for the enemy to go from each position.
+    
+    public Enemy(EnemyManager enemyManager)
+    {
+        this.enemyManager = enemyManager;
+    }
+
+
+    // Call every frame from enemy manager.
+    public void Update(float dt)
+    {
+        timer += dt;            // Update the current timer.
+        Move();                 // Move the enemy.
+    }
+
+
+
+
+    // Move the enemy along the path in the enemy manager.
+    private void Move()
+    {
+        float percent = (timer - timePerPosition * (targetPositionIndex - 1)) / timer;      // Calculate the percentage between the last and current target postion
+        position = Vector3.Lerp(enemyManager.enemyPath[targetPositionIndex - 1],            // LERP between the last and current target position.
+                                enemyManager.enemyPath[targetPositionIndex],
+                                percent);
+        
+        // If the enemy is sufficiently close to the target position, switch target position.
+        if(Vector3.SqrMagnitude(position - enemyManager.enemyPath[targetPositionIndex]) < 1e-3)
+        {
+            targetPositionIndex += 1;
+        }
+    }
+}
---
+++ UnityProject/Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Enemy Manager class
+// - Dummy container for data regarding each enemy.
+// - The enemies will do the calculations themselves.
+
+public class EnemyManager : MonoBehaviour
+{
+    private List<Enemy> enemies;        // List of all the current enemies on screen.
+    private int roundCounter;           // Determines which enemies to spawn.
+
+    public List<Vector3> enemyPath;     // The path of points that the enemies LERP between.
+
+    void Start()
+    {
+        // Set default values.
+        enemies.Clear();
+        roundCounter = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(Enemy enemy in enemies) { enemy.Update(Time.deltaTime); }
+    }
+}
