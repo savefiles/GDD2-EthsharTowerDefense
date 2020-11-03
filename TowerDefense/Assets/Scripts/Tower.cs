a6eaacb774139774e6a9af1cc2aabb36f17970ac
Chris Hambacher <hambcastlesen12@gmail.com>
2020-11-03 09:28:37 -0500

    Changed the project name for clarity sake

---
+++ TowerDefense/Assets/Scripts/Tower.cs
@@ -0,0 +1,350 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public enum TowerType {
+    Type_Archer,
+    Type_Magic,
+    Type_Siege
+}
+
+public enum TowerLevel {
+    Level_1,
+    Level_2,
+    Level_3a,
+    Level_3b,
+    Level_4a,
+    Level_4b
+}
+
+public enum AttackType {
+    Type_Hitscan,
+    Type_Projectile
+}
+
+public class Tower {
+    //  ~Tower Variables
+    public GameObject towerActual;
+
+    public TowerType towerType;            //  Type of Tower (Archer, Magic, Siege)
+    public TowerType TowerType => towerType;
+
+    public TowerLevel towerLevel;          //  Level of Tower (1, 2, 3a, 3b, 4a, 4b)
+    public TowerLevel TowerLevel => towerLevel;
+
+    //  Audio Variables
+    private AudioSource towerSound;
+
+    //  Combat Variables
+    private float towerRange;
+    private float towerImpactRange = 0.2f;
+
+    private int towerFireRate;              //  Shots per minute of tower
+    private float towerCoolBase;            //  Reset value of cooldown
+    private float towerCoolCurr;            //  Current value of cooldown
+
+    private AttackType towerAttack;         //  Type of Attack (Hitscan, Projectile)
+    private int towerDamage;                //  Amount of Damage
+
+    //  Enemy Variables
+    private List<Transform> enemyList;      //  Grabs all enemies in range
+    private Transform towerTarget;          //  Current tower target
+
+    public void SetupTower(GameObject pTower, TowerType pType) {
+        towerActual = pTower;
+
+        switch (pType) {
+            //  Part - Archer Tower Setup
+            case TowerType.Type_Archer:
+                towerType = TowerType.Type_Archer;
+                towerAttack = AttackType.Type_Hitscan;
+                break;
+
+            //  Part - Magisc Tower Setup
+            case TowerType.Type_Magic:
+                towerType = TowerType.Type_Magic;
+                towerAttack = AttackType.Type_Hitscan;
+                break;
+
+            //  Part - Archer Tower Setup
+            case TowerType.Type_Siege:
+                towerType = TowerType.Type_Siege;
+                towerAttack = AttackType.Type_Projectile;
+                break;
+        }
+
+        //  Part - Enemy Setup
+        enemyList = new List<Transform>();
+    }
+
+    public void InitializeTower() {
+        //  Part - Tower Base
+        switch (towerLevel) {
+            case TowerLevel.Level_1:
+                towerActual.transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().sprite = GameManager.instance.towerManager.towerBase_Level1;
+                break;
+
+            case TowerLevel.Level_2:
+                towerActual.transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().sprite = GameManager.instance.towerManager.towerBase_Level2;
+                break;
+
+            case TowerLevel.Level_3a:
+            case TowerLevel.Level_3b:
+                towerActual.transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().sprite = GameManager.instance.towerManager.towerBase_Level3;
+                break;
+
+            case TowerLevel.Level_4a:
+            case TowerLevel.Level_4b:
+                //transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().sprite = GameManager.instance.towerManager.towerBase_Level4;
+                break;
+        }
+
+        //  Part - Tower Upper and Sound
+        switch (towerType) {
+            case TowerType.Type_Archer:
+                towerActual.transform.GetChild(1).gameObject.GetComponent<SpriteRenderer>().sprite = GameManager.instance.towerManager.towerUpper_Archer;
+                towerActual.GetComponent<AudioSource>().clip = GameManager.instance.towerManager.sound_Archer;
+                break;
+
+            case TowerType.Type_Magic:
+                towerActual.transform.GetChild(1).gameObject.GetComponent<SpriteRenderer>().sprite = GameManager.instance.towerManager.towerUpper_Magic;
+                towerActual.GetComponent<AudioSource>().clip = GameManager.instance.towerManager.sound_Magic;
+                break;
+
+            case TowerType.Type_Siege:
+                towerActual.transform.GetChild(1).gameObject.GetComponent<SpriteRenderer>().sprite = GameManager.instance.towerManager.towerUpper_Siege;
+                towerActual.GetComponent<AudioSource>().clip = GameManager.instance.towerManager.sound_Siege;
+                break;
+        }
+    }
+
+    public void Update() {
+        TowerTarget();
+
+        if (towerCoolCurr <= 0) {
+            if (towerTarget != null) {
+                TowerAttack();
+            }
+
+            towerCoolCurr = towerCoolBase;
+        }
+
+        else {
+            towerCoolCurr -= Time.deltaTime;
+        }
+    }
+
+    //  SubMethod of Update - Tower Target
+    private void TowerTarget() {
+        //  Part - Reset Target List
+        if (towerTarget == null) {
+            enemyList.Clear();
+
+            foreach (Enemy enemy in GameManager.instance.enemyManager.enemies) {
+                if (Vector2.Distance(enemy.gameObject.transform.position, towerActual.transform.position) <= towerRange) {
+                    enemyList.Add(enemy.gameObject.transform);
+                }
+            }
+
+            //  Part - Target Enemy
+            for (int i = 0; i < enemyList.Count; i++) {
+                //  MinorPart - First Enemy
+                if (i == 0) {
+                    towerTarget = enemyList[i];
+                }
+
+                //  SubPart - Enemy Ahead (Further Waypoint)
+                else if (enemyList[i].GetComponent<EnemyMB>().enemyRef.targetPositionIndex > towerTarget.GetComponent<EnemyMB>().enemyRef.targetPositionIndex) {
+                    towerTarget = enemyList[i];
+                }
+
+                //  SubPart - Enemy Ahead (Same Waypoint)
+                else if (enemyList[i].GetComponent<EnemyMB>().enemyRef.timeSinceLastPoint > towerTarget.GetComponent<EnemyMB>().enemyRef.timeSinceLastPoint) {
+                    towerTarget = enemyList[i];
+                }
+            }
+        }
+
+        //  Part - Tower Rotate
+        if (towerTarget != null) {
+            Vector3 diff = towerTarget.position - towerActual.transform.position;
+
+            diff.Normalize();
+
+            float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+            towerActual.transform.GetChild(1).rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
+        }
+    }
+
+    //  SubMethod of Update - Tower Attack
+    private void TowerAttack() {
+        //  Part - Hitscan Tower (Archer, Magic)
+        if (towerAttack == AttackType.Type_Hitscan) {
+            towerTarget.GetComponent<EnemyMB>().enemyRef.TakeDamage(towerDamage);
+
+            if (towerTarget.GetComponent<EnemyMB>().enemyRef.markedForDeletion == true) {
+                towerTarget = null;
+            }
+        }
+
+        //  Part - Projectile Tower (Siege)
+        else if (towerAttack == AttackType.Type_Projectile) {
+            Vector2 impactPos = towerTarget.transform.position;
+
+            foreach (Enemy enemy in GameManager.instance.enemyManager.enemies) {
+                if (Vector2.Distance(enemy.gameObject.transform.position, impactPos) <= towerImpactRange) {
+                    enemy.gameObject.GetComponent<EnemyMB>().enemyRef.TakeDamage(towerDamage);
+
+                    if (towerTarget.GetComponent<EnemyMB>().enemyRef.markedForDeletion == true) {
+                        towerTarget = null;
+                    }
+                }
+            }
+        }
+
+        towerActual.GetComponent<AudioSource>().Play();
+    }
+
+    //  MainMethod - Upgrade Tower Main (param Tower Level)
+    public void UpgradeTower_Main(TowerLevel pLevel, TowerManager pManager) {
+        towerLevel = pLevel;
+
+        switch (towerType) {
+            //  Part - Archer Tower Upgrade
+            case TowerType.Type_Archer:
+                switch (towerLevel) {
+                    //  SubPart - Archer Tower Level 1
+                    case TowerLevel.Level_1:
+                        UpgradeTower_Sub(2f, 60, 10);
+                        pManager.manaCurr -= pManager.costArcher1;
+                        break;
+
+                    //  SubPart - Archer Tower Level 2
+                    case TowerLevel.Level_2:
+                        UpgradeTower_Sub(2f, 60, 10);
+                        pManager.manaCurr -= pManager.costArcher2;
+                        break;
+
+                    //  SubPart - Archer Tower Level 3a
+                    case TowerLevel.Level_3a:
+                        UpgradeTower_Sub(2f, 60, 10);
+                        pManager.manaCurr -= pManager.costArcher3a;
+                        break;
+
+                    //  SubPart - Archer Tower Level 3b
+                    case TowerLevel.Level_3b:
+                        UpgradeTower_Sub(2f, 60, 10);
+                        pManager.manaCurr -= pManager.costArcher3b;
+                        break;
+
+                    //  SubPart - Archer Tower Level 4a
+                    case TowerLevel.Level_4a:
+                        UpgradeTower_Sub(2f, 60, 10);
+                        pManager.manaCurr -= pManager.costArcher4a;
+                        break;
+
+                    //  SubPart - Archer Tower Level 4b
+                    case TowerLevel.Level_4b:
+                        UpgradeTower_Sub(2f, 60, 10);
+                        pManager.manaCurr -= pManager.costArcher4b;
+                        break;
+                }
+                break;
+
+            //  Part - Magic Tower Upgrade
+            case TowerType.Type_Magic:
+                switch (towerLevel) {
+                    //  SubPart - Magic Tower Level 1
+                    case TowerLevel.Level_1:
+                        UpgradeTower_Sub(2f, 60, 10);
+                        pManager.manaCurr -= pManager.costMagic1;
+                        break;
+
+                    //  SubPart - Magic Tower Level 2
+                    case TowerLevel.Level_2:
+                        UpgradeTower_Sub(2f, 60, 10);
+                        pManager.manaCurr -= pManager.costMagic2;
+                        break;
+
+                    //  SubPart - Magic Tower Level 3a
+                    case TowerLevel.Level_3a:
+                        UpgradeTower_Sub(2f, 60, 10);
+                        pManager.manaCurr -= pManager.costMagic3a;
+                        break;
+
+                    //  SubPart - Magic Tower Level 3b
+                    case TowerLevel.Level_3b:
+                        UpgradeTower_Sub(2f, 60, 10);
+                        pManager.manaCurr -= pManager.costMagic3b;
+                        break;
+
+                    //  SubPart - Magic Tower Level 4a
+                    case TowerLevel.Level_4a:
+                        UpgradeTower_Sub(2f, 60, 10);
+                        pManager.manaCurr -= pManager.costMagic4a;
+                        break;
+
+                    //  SubPart - Magic Tower Level 4b
+                    case TowerLevel.Level_4b:
+                        UpgradeTower_Sub(2f, 60, 10);
+                        pManager.manaCurr -= pManager.costMagic4b;
+                        break;
+                }
+                break;
+
+            //  Part - Siege Tower Upgrade
+            case TowerType.Type_Siege:
+                switch (towerLevel) {
+                    //  SubPart - Siege Tower Level 1
+                    case TowerLevel.Level_1:
+                        UpgradeTower_Sub(2f, 60, 10);
+                        pManager.manaCurr -= pManager.costSiege1;
+                        break;
+
+                    //  SubPart - Siege Tower Level 2
+                    case TowerLevel.Level_2:
+                        UpgradeTower_Sub(2f, 60, 10);
+                        pManager.manaCurr -= pManager.costSiege2;
+                        break;
+
+                    //  SubPart - Siege Tower Level 3a
+                    case TowerLevel.Level_3a:
+                        UpgradeTower_Sub(2f, 60, 10);
+                        pManager.manaCurr -= pManager.costSiege3a;
+                        break;
+
+                    //  SubPart - Siege Tower Level 3b
+                    case TowerLevel.Level_3b:
+                        UpgradeTower_Sub(2f, 60, 10);
+                        pManager.manaCurr -= pManager.costSiege3b;
+                        break;
+
+                    //  SubPart - Siege Tower Level 4a
+                    case TowerLevel.Level_4a:
+                        UpgradeTower_Sub(2f, 60, 10);
+                        pManager.manaCurr -= pManager.costSiege4a;
+                        break;
+
+                    //  SubPart - Siege Tower Level 4b
+                    case TowerLevel.Level_4b:
+                        UpgradeTower_Sub(2f, 60, 10);
+                        pManager.manaCurr -= pManager.costSiege4b;
+                        break;
+                }
+                break;
+        }
+
+        InitializeTower();
+    }
+
+    //  SubMethod of UpgradeTower_Main - Upgrade Tower Sub (param Fire Rate, Damage)
+    private void UpgradeTower_Sub(float pRange, int pRate, int pDamage) {
+        //  Part - Combat Setup
+        towerRange = pRange;
+
+        towerFireRate = pRate;
+        towerCoolBase = 60 / towerFireRate;
+        towerCoolCurr = 0;
+
+        towerDamage = pDamage;
+    }
+}
---
+++ TowerDefense/Assets/Scripts/TowerManager.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerManager {
+
+    //  _Unity Variables
+    private GameObject objTowerPrefab;
+    private GameObject objTowerSpawn;
+
+    private GameObject levelMap;
+
+    //  Audio Variables
+    public AudioClip sound_Archer;
+    public AudioClip sound_Magic;
+    public AudioClip sound_Siege;
+
+    //  Image Variables
+    public Sprite towerBase_Level1;
+    public Sprite towerBase_Level2;
+    public Sprite towerBase_Level3;
+    //public Sprite towerBase_Level4;
+
+    public Sprite towerUpper_Archer;
+    public Sprite towerUpper_Magic;
+    public Sprite towerUpper_Siege;
+
+    //  Tower Variables
+    private List<TowerMB> towerList;
+    public List<TowerMB> TowerList => towerList;
+
+    public int manaCurr;
+
+    public int costArcher1 = 100;
+    public int costArcher2 = 200;
+    public int costArcher3a = 350;
+    public int costArcher3b = 400;
+    public int costArcher4a = 500;
+    public int costArcher4b = 600;
+
+    public int costMagic1 = 100;
+    public int costMagic2 = 200;
+    public int costMagic3a = 350;
+    public int costMagic3b = 400;
+    public int costMagic4a = 500;
+    public int costMagic4b = 600;
+
+    public int costSiege1 = 100;
+    public int costSiege2 = 200;
+    public int costSiege3a = 350;
+    public int costSiege3b = 400;
+    public int costSiege4a = 500;
+    public int costSiege4b = 600;
+
+    public TowerManager() {
+        //  Part - _Unity Setup
+        objTowerPrefab = Resources.Load<GameObject>("Prefabs/Tower");
+        objTowerSpawn = GameObject.Find("Tower_Spawn");
+
+        levelMap = GameObject.Find("Level Map");
+
+        //  Part - Audio Setup
+        sound_Archer = Resources.Load<AudioClip>("Audio/Sound_Archer");
+        sound_Magic = Resources.Load<AudioClip>("Audio/Sound_Magic");
+        sound_Siege = Resources.Load<AudioClip>("Audio/Sound_Siege");
+
+        //  Part - Image Setup
+        towerBase_Level1 = Resources.Load<Sprite>("Sprites/TowerBase_Level1");
+        towerBase_Level2 = Resources.Load<Sprite>("Sprites/TowerBase_Level2");
+        towerBase_Level3 = Resources.Load<Sprite>("Sprites/TowerBase_Level3");
+        //towerBase_Level4 = Resources.Load<Sprite>("Sprites/TowerBase_Level4");
+
+        towerUpper_Archer = Resources.Load<Sprite>("Sprites/TowerUpper_Archer");
+        towerUpper_Magic = Resources.Load<Sprite>("Sprites/TowerUpper_Magic");
+        towerUpper_Siege = Resources.Load<Sprite>("Sprites/TowerUpper_Siege");
+
+        //  Part - Tower Setup
+        towerList = new List<TowerMB>();
+
+        manaCurr = 500;
+    }
+
+    public void Update() {
+        foreach(TowerMB tower in towerList) {
+            tower.towerRef.Update();
+        }
+    }
+
+    public bool SpawnCheck(float pSize) {
+        return SpawnLevelCheck() && SpawnTowerCheck(pSize) && SpawnPathCheck(pSize);
+    }
+
+    public bool SpawnLevelCheck() {
+        Vector3 mousePos = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, levelMap.transform.position.z);
+        return levelMap.GetComponent<Renderer>().bounds.Contains(mousePos);
+    }
+
+    public bool SpawnTowerCheck(float pSize) {
+        foreach(TowerMB tower in towerList) {
+            if (Vector2.Distance(tower.towerRef.towerActual.transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition)) < pSize) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private bool SpawnPathCheck(float pSize) {
+        List<Vector3> pathPoints = GameManager.instance.levelManager.currentLevel.worldPath;
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        for (int i = 0; i < pathPoints.Count - 1; i++) {
+            //  Part - Horizontal Section
+            if (pathPoints[i].y == pathPoints[i + 1].y) {
+                Rect horzPath = Rect.zero;
+
+                //  SubPart - Right Path
+                if (pathPoints[i].x < pathPoints[i + 1].x) {
+                    horzPath = new Rect(pathPoints[i].x, pathPoints[i].y - (pSize / 2), Math.Abs(pathPoints[i].x - pathPoints[i + 1].x) + pSize, 3 * (pSize / 2));
+                }
+
+                //  SubPart - Left Path
+                else if (pathPoints[i].x > pathPoints[i + 1].x) {
+                    horzPath = new Rect(pathPoints[i + 1].x, pathPoints[i + 1].y - (pSize / 2), Math.Abs(pathPoints[i].x - pathPoints[i + 1].x) + pSize, 3 * (pSize / 2));
+                }
+
+                if (horzPath.Contains(mousePos)) {
+                    return false;
+                }
+            }
+
+            //  Part - Vertical Section
+            else if (pathPoints[i].x == pathPoints[i + 1].x) {
+                Rect vertPath = Rect.zero;
+
+                //  SubPart - Bottom Path
+                if (pathPoints[i].y < pathPoints[i + 1].y) {
+                    vertPath = new Rect(pathPoints[i].x - (pSize / 2), pathPoints[i].y, 3 * (pSize / 2), Math.Abs(pathPoints[i].y - pathPoints[i + 1].y) + pSize);
+                }
+
+                //  SubPart - Top Path
+                else if (pathPoints[i].y > pathPoints[i + 1].y) {
+                    vertPath = new Rect(pathPoints[i + 1].x - (pSize / 2), pathPoints[i + 1].y, 3 * (pSize / 2), Math.Abs(pathPoints[i].y - pathPoints[i + 1].y) + pSize);
+                }
+
+                if (vertPath.Contains(mousePos)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public void SpawnTower(TowerType pType) {
+        //  Part - Instantiate Tower
+        Vector3 spawnPos = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, -1);
+        GameObject towerObj = GameObject.Instantiate(objTowerPrefab, spawnPos, Quaternion.identity, objTowerSpawn.transform);
+
+        towerObj.GetComponent<TowerMB>().towerRef = new Tower();
+
+        //  Part - Setup Tower
+        towerObj.GetComponent<TowerMB>().towerRef.SetupTower(towerObj, pType);
+        towerObj.GetComponent<TowerMB>().towerRef.UpgradeTower_Main(TowerLevel.Level_1, this);
+        towerObj.GetComponent<TowerMB>().towerRef.InitializeTower();
+
+        //  Part - Add Tower to towerList
+        towerList.Add(towerObj.GetComponent<TowerMB>());
+    }
+}
